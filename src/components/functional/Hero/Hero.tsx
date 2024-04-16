29ddb8caefe2beb41c39c3b0538520eb999380c3
Amelia <amelia.feliu06@gmail.com>
2024-04-16 11:07:33 -0400

    actualizacion 16/04 11:07AM

--- src/components/functional/Hero/Hero.tsx
+++ src/components/functional/Hero/Hero.tsx
@@ -4,20 +4,17 @@ import styles from "./Hero.module.scss";
 
 import HeroBackground from "@/components/ui/HeroBackground";
 import Logo from "@/components/ui/Logo";
-import Menu from "@/components/ui/Menu";
 import Message from "@/components/ui/Message";
 import HamburgerMenu from "@/components/ui/HamburgerMenu/HamburgerMenu";
-
 
 const Hero = () => {
   return (
     <HeroBackground>
       <div className={styles.nav}>
-        <Logo width="252px" height="53px"  />
+        <Logo width="252px" height="53px" />
         <HamburgerMenu />
-        <Menu />
       </div>
-      
+
       <Message />
     </HeroBackground>
   );
--- src/components/ui/HamburgerMenu/HamburgerMenu.tsx
+++ src/components/ui/HamburgerMenu/HamburgerMenu.tsx
@@ -1,55 +1,45 @@
 
-import { useState, useEffect } from 'react';
+
+// components/HamburgerMenu.tsx
+import React, { useState, useEffect, useRef } from 'react';
 import styles from './HamburgerMenu.module.scss';
 
-const HamburgerMenu = () => {
-  const [isMobile, setIsMobile] = useState<boolean>(false);
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  useEffect(() => {
-    const checkIsMobile = () => {
-      setIsMobile(window.innerWidth <= 768); // Define el ancho máximo para considerar dispositivos móviles
-    };
-
-    checkIsMobile();
-
-    window.addEventListener('resize', checkIsMobile);
-
-    return () => {
-      window.removeEventListener('resize', checkIsMobile);
-    };
-  }, []);
+const HamburgerMenu: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
-  if (!isMobile) {
-    return null; // No renderizar el menú hamburguesa si no es un dispositivo móvil
-  }
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   return (
-    <div className={styles.container}>
-      <div className={styles.hamburger} onClick={toggleMenu}>
-        <div className={styles.line}></div>
-        <div className={styles.line}></div>
-        <div className={styles.line}></div>
-      </div>
-
-      {isOpen && (
-        <div className={styles.menu}>
-          <ul>
-          <li>Novedades</li>
-            <li>Servicios</li>
-            <li>Sobre Nosotros</li>
-            <li>Ubicación</li>
-            <li>Contacto</li>
-          </ul>
-        </div>
-      )}
+    <div className={styles.menu} ref={menuRef}>
+      <ul className={`${styles.navLinks} ${isOpen ? styles.show : ''}`}>
+       <li><a href="#" >Novedades</a></li>
+        <li><a href="#" >Servicios</a></li>
+        <li><a href="#" >Sobre Nosotros </a></li>
+        <li><a href="#" >Ubicación</a></li>
+        <li><a href="#" >Contacto</a></li>
+      </ul>
+      <button className={styles.toggleButton} onClick={toggleMenu}>
+        ☰
+      </button>
     </div>
   );
 };
 
 export default HamburgerMenu;
-
