d1e34b419b19734358332a96bf1474ff33806cfa
Amelia <amelia.feliu06@gmail.com>
2024-04-16 23:16:46 -0400

    responsive

--- src/components/functional/Score/Score.tsx
+++ src/components/functional/Score/Score.tsx
@@ -5,13 +5,14 @@ import CardScore from '@/components/ui/CardScore';
 
 const Score = () => {
   return (
+    <div className={styles.background}>
     <div className={styles.score}>
       <CardScore img='./img/score/star.png' width='65px' height='65px' title='+22 ' text='Años de Experiencia'></CardScore>
-      <CardScore img='./img/score/people.png' width='63px' height='55px' title='+900M' text='Clientes Finales'></CardScore>
+      <CardScore img='./img/score/people.png' width='65px' height='65px' title='+900M' text='Clientes Finales'></CardScore>
       <CardScore img='./img/score/hands.png' width='65px' height='65px' title='+20' text='Alianzas y Clientes'></CardScore>
-      <CardScore img='./img/score/clock.png' width='56px' height='53px' title='24hrs' text='Presentes en todo Chile'></CardScore>
+      <CardScore img='./img/score/clock.png' width='65px' height='65px' title='24hrs' text='Presentes en todo Chile'></CardScore>
     </div>
-    
+    </div>
    
   );
 };
---
+++ src/components/ui/Wallpaper/Wallpaper.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import styles from "./Wallpaper.module.scss";
+interface IWallpaper {
+  img: string;
+  width: string;
+  height: string;
+  children: any;
+}
+const Wallpaper = ({ img, width, height, children}: IWallpaper) => {
+  return (
+    <div className={styles.wallpaper}>
+      <img className={styles.img}
+        src={`${img}`}
+        alt=""
+        style={{
+          width: "100%",
+          height: `calc(100vw * ${height} / ${width})`,
+        }}
+      />
+      {children}
+    </div>
+  );
+};
+
+export default Wallpaper;
